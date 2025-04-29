ce5ada5e9a005279edda94735becb2d397b2ad8a
elsiej013 <elsiejim@usc.edu>
2025-04-29 14:51:51 -0700

    Updated routes to include path for Study creation page, new pages can be added.

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,10 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    {
+      path: "study-creation", 
+      file: "routes/study-creation-page.tsx" 
+    }
+  ] satisfies RouteConfig;
+
