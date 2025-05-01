70b4f60c4aa6d3672e15cfc4c93d2bf310b93ed8
snbreen <139181779+snbreen@users.noreply.github.com>
2025-05-01 14:48:46 -0700

    Update AdminUser.java

--- backend/src/main/java/com/study_group_matcher/model/AdminUser.java
+++ backend/src/main/java/com/study_group_matcher/model/AdminUser.java
@@ -3,42 +3,20 @@ package com.study_group_matcher.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.study_group_matcher.model.Role;
 
-import jakarta.persistence.*;
-
-@Entity
-@PrimaryKeyJoinColumn(name = "user_id")
 public class AdminUser extends User {
-    
-    @OneToMany(mappedBy = "admin")
-    private List<StudyGroup> groupsCreated = new ArrayList<>();
-    
-    public AdminUser() {
-        this.setRole(Role.ADMIN);
-    }
-    
-    private void setRole(Role admin) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setRole'");
-    }
+    public AdminUser(){
 
-    // Original Admin Methods
-    public StudyGroup createGroup(String name, String course) {
-        StudyGroup group = new StudyGroup(name, course, this);
-        groupsCreated.add(group);
-        return group;
     }
-    
-    public void addToGroup(User user, StudyGroup group) {
-        if (groupsCreated.contains(group)) {
-            user.joinGroup(group);
-        }
+    public AdminUser(List<StudyGroup> gc){
+        groupsCreated = gc;
     }
-    
-    public void removeFromGroup(User user, StudyGroup group) {
-        if (groupsCreated.contains(group)) {
-            user.leaveGroup(group);
-        }
+    public List<StudyGroup> getGroupsCreated(){
+        return groupsCreated;
     }
+    public void setGroupsCreated(List<StudyGroup> gc){
+        groupsCreated = gc;
+    }
+    private List<StudyGroup> groupsCreated;
+
 }
