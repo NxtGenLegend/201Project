9200294fd317eab78bf9733afeb609e6eca0ece6
Thais Chung <tschung@usc.edu>
2025-05-01 15:57:53 -0700

    Update CreateSG.sql

--- backend/src/main/java/com/study_group_matcher/db/CreateSG.sql
+++ backend/src/main/java/com/study_group_matcher/db/CreateSG.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS STUDY_GROUP_MATCHER;
 USE STUDY_GROUP_MATCHER;
 
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(225) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE Users (
     last_login_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE StudyGroup (
+CREATE TABLE IF NOT EXISTS StudyGroup (
     study_group_id INT PRIMARY KEY AUTO_INCREMENT,
     admin_id INT NOT NULL,
     privacy ENUM('PUBLIC', 'PRIVATE') NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE StudyGroup (
     FOREIGN KEY (admin_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE StudyGroupMembers (
+CREATE TABLE IF NOT EXISTS StudyGroupMembers (
     study_group_id INT NOT NULL,
     user_id INT NOT NULL,
     group_role ENUM('ADMIN', 'MEMBER') NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE StudyGroupMembers (
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Message (
+CREATE TABLE IF NOT EXISTS Message (
     message_id INT PRIMARY KEY AUTO_INCREMENT,
     sender_id INT NOT NULL,
     recipient_id INT NOT NULL,
@@ -39,25 +39,25 @@ CREATE TABLE Message (
     FOREIGN KEY (recipient_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Inbox (
-    inbox_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    sender_id INT NOT NULL,
-    status ENUM('PENDING', 'ACCEPTED', 'DECLINED') NOT NULL,
-    notification ENUM('NOTIFIED', 'SEEN'),
-    contents TINYTEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (sender_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE Invitations (
+CREATE TABLE IF NOT EXISTS Invitations (
   invitation_id INT PRIMARY KEY AUTO_INCREMENT,
   study_group_id INT NOT NULL,
   user_id INT NOT NULL,
+  StudyGroupuser_id INT NOT NULL,
   status ENUM('PENDING', 'ACCEPTED', 'DECLINED') NOT NULL DEFAULT 'PENDING',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME,
   FOREIGN KEY (study_group_id) REFERENCES StudyGroup(study_group_id),
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
+
+CREATE TABLE IF NOT EXISTS Inbox (
+    inbox_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    sender_id INT NOT NULL,
+    message_id INT,
+    invitation_id INT,
+    FOREIGN KEY (message_id) REFERENCES Message(message_id),
+    FOREIGN KEY (invitation_id) REFERENCES Invitations(invitation_id)
+);
+
