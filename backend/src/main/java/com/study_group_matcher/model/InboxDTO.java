d16c11f198942d3551f85ce3bc1386f64a9f765c
Thais Chung <tschung@usc.edu>
2025-05-01 18:10:47 -0700

    Create InboxDTO.java

---
+++ backend/src/main/java/com/study_group_matcher/model/InboxDTO.java
@@ -0,0 +1,25 @@
+public class InboxDTO {
+    private Long messageId;
+    private Long invitationId;
+
+    public InboxDTO(Long messageId, Long invitationId) {
+        this.messageId = messageId;
+        this.invitationId = invitationId;
+    }
+    
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public Long getInvitationId() {
+        return invitationId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public void setInvitationId(Long invitationId) {
+        this.invitationId = invitationId;
+    }
+}
